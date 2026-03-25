3ca764fbbe91c47b91d0556664b49c95f7155ce3
mohamedfazil2315 <mohamedfazil2315@gmail.com>
2026-03-25 13:07:25 +0530

    Delete myweb directory

--- myweb/app1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns=[
-    path("home",views.home,name="home"),
-    path("regpage",views.regpage,name="regpage"),
-    path("cdisplay",views.cdisplay,name="cdisplay"),
-]
